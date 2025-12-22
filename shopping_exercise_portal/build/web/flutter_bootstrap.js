04c08941cb96df4b873ab5bcde8613809af97682
Julio León <julioleon2004@gmail.com>
2025-12-22 16:42:35 -0400

    Build portal Flutter web (release)

--- shopping_exercise_portal/build/web/flutter_bootstrap.js
+++ shopping_exercise_portal/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1925544385"
+    serviceWorkerVersion: "3667228022"
   }
 });
