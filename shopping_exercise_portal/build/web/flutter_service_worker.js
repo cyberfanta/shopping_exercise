dcb5d735878e04bdc2f6c750787f869eaf950d35
Julio León <julioleon2004@gmail.com>
2025-12-22 16:44:01 -0400

    Build portal Flutter web (release)

--- shopping_exercise_portal/build/web/flutter_service_worker.js
+++ shopping_exercise_portal/build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "09c5d1d89c0a88425ae756d5bf4bfb2b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "bf1ed0b18a92b2b7f222bb87cb4638d8",
+"flutter_bootstrap.js": "e6ee2cc2edf57bed8a392a58523775d5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
