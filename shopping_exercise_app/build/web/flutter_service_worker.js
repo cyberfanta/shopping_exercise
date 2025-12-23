1b08199ce1f8da6ca4e1cb402dea24619405f0b0
Julio León <julioleon2004@gmail.com>
2025-12-22 22:05:12 -0400

    Build app Flutter web (release)

--- shopping_exercise_app/build/web/flutter_service_worker.js
+++ shopping_exercise_app/build/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "fe1ee92edea6b13f40f61664069ab6e0",
+"flutter_bootstrap.js": "6102135ca80ebca1c2b0519507124da7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7f9adbe9ee8c54e85f40649b61fbcd4c",
 "/": "7f9adbe9ee8c54e85f40649b61fbcd4c",
-"main.dart.js": "73ecb93ab7666a8574e957a2a7746b9d",
+"main.dart.js": "ca16208225ae3c0e5879b0b2e2f04e8c",
 "manifest.json": "1d44da60e8f4fdc4c03a039a123e313d",
 "version.json": "8980476c0be0fe18544752c477e0e625"};
 // The application shell files that are downloaded before a service worker can
